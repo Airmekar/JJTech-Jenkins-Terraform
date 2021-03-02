e418d2c8fb638022a29fcea71ad80d420b97e639
Harris <Airmekar@Outlook.com>
2021-03-02 00:36:05 -0500

    updated bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "cicd-terraform-nv"
+    bucket = "cicd-teraform-nv"
     key    = "state/jjtech.tfstate"
     region = "us-east-1"
     profile = "DevOps-3plePoint"
